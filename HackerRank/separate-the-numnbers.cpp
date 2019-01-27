fce03b2d5bca8659d3ce0b5fe954c16f6d0cd7f3
Rafel <rafalopezlizana@gmail.com>
2019-01-26 21:55:50 -0500

    26

--- HackerRank/separate-the-numnbers.cpp
+++ HackerRank/separate-the-numnbers.cpp
@@ -4,51 +4,58 @@ typedef long long Long;
 using namespace std;
 
 
-Long numero(string a){    
-    cout<<a<<endl;
-    return stoi(a);
-}
-
-Long numeroDigitos (Long n){
-    Long can=0;
-    if(n==0)return 1;
-    while(n>0){
-        can++;
-        n=n/10;
+string numeroSiguiente (string n){
+    int tamanio = n.size(),cant=0;
+    for (int i= tamanio-1 ;i>=0;i--){
+        if( n[i]=='9') cant++;
+        else break;
     }
-    return can;
+    if(cant!= tamanio){
+        for (int i= tamanio-1 ;i>=tamanio-cant;i--){
+            n[i]='0';
+        }
+        n[tamanio-cant-1]=char(n[tamanio-cant-1]+1);
+    }
+    else{     
+        para(i,tamanio){
+            n[i]='0';
+        }
+        n.insert(n.begin(),'1');
+    }
+    return n;
 }
 
 string separando(string c){ 
     
     for(Long i=1;i<=c.size()/2;i++){
         bool encon=true;
-        Long min=numero(c.substr(0,i));
+        string min = c.substr(0,i);
 
-        Long numDigitosAct=numeroDigitos(min);
-        Long numDigitosSig=numeroDigitos(min+1);
-        Long numeroPrevio=numDigitosAct;       
+        Long numDigitosAct= min.size();
 
-        for(Long k=0; k<(Long)c.size()-numDigitosAct;k += numeroPrevio){            
-            Long numeroActual = numero(c.substr(k,numDigitosAct));
-            Long numeroSiguiente =numero(c.substr( k + numDigitosAct , numDigitosSig ));          
-            if  ( numeroActual+1 != numeroSiguiente){
-                encon=false;
-                break;
-            }else{
-                numeroPrevio=numDigitosAct;
-                numDigitosAct=numDigitosSig;
-                numDigitosSig=numeroDigitos(numeroSiguiente+1);                
+        Long numDigitosSig=numeroSiguiente(min).size();
+
+        Long numeroPrevio=numDigitosAct; 
+        if(min[0] != '0'){
+            for(Long k=0; k<(Long)c.size()-numDigitosAct;k += numeroPrevio){  
+                string numeroActual = c.substr(k,numDigitosAct);
+                string numSiguiente =c.substr( k + numDigitosAct , numDigitosSig ); 
+                string verdaderoNumeroSiguiente = numeroSiguiente(numeroActual);
+                if  ( verdaderoNumeroSiguiente.compare(numSiguiente) != 0 ){
+                    encon=false;
+                    break;
+                }else{
+                    numeroPrevio=numDigitosAct;
+                    numDigitosAct=numDigitosSig;                            
+                    numDigitosSig=numeroSiguiente(numSiguiente).size();                
+                }
             }
-            
-        }
 
-        if(encon){
-            string resp;
-            resp.append("YES ");
-            resp.append(to_string(min));
-            return resp;
-        }
+                if(encon){
+                string resp;resp.append("YES ");resp.append(min);
+                return resp;
+            }
+        }      
 
     }    
     return "NO";
